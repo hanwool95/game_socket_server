6479578d04b7fb208a29a1bd32db739882e35378
hanwool95 <hanwool95@snu.ac.kr>
2024-10-03 01:59:19 +0900

    FEATURE: INIT Server

---
+++ src/game/game.gateway.ts
@@ -0,0 +1,40 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class GameGateway
+    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('GameGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('chat')
+  handleMessage(client: Socket, payload: string): void {
+    this.logger.log(`Message from ${client.id}: ${payload}`);
+    this.server.emit('chat', payload);
+  }
+}
