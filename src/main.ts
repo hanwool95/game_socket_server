cd57bbb0c5887f326cddd66ffda141bde7ef4ca0
hanwool95 <hanwool95@snu.ac.kr>
2024-10-03 17:10:21 +0900

    FEATURE: add cors on app

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://www.ceasar.kr', // 허용할 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 메서드
+    credentials: true, // 자격 증명 포함
+  });
+
   await app.listen(4000);
 }
 bootstrap();
