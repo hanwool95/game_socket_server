cc6e416c5cf0a2c00e9fceaad8b7d493ccfb0409
hanwool95 <hanwool95@snu.ac.kr>
2024-10-06 20:39:50 +0900

    FEATURE: prisma

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,10 +1,11 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-export class PrismaService extends PrismaClient {
-  constructor() {
-    super();
-  }
-
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
   }
